0ec8532ebc1faeba4a7349f8312b0b21d1ed8fd2
Yiheng Shu <lzw429@qq.com>
2017-08-12 16:27:44 +0800

    Update SMO

--- ex6/svmTrain.m
+++ ex6/svmTrain.m
@@ -2,7 +2,8 @@ function [model] = svmTrain(X, Y, C, kernelFunction, ...
                             tol, max_passes)
 %SVMTRAIN Trains an SVM classifier using a simplified version of the SMO 
 %algorithm. 
-%   使用简单的SMO算法训练一个支持向量机分类器。
+%   使用简单版的 SMO 算法（序列最小优化算法）训练一个支持向量机分类器。
+%	本代码是求解 SVM 的核心部分，是 ex6 最值得研究的代码。
 %   [model] = SVMTRAIN(X, Y, C, kernelFunction, tol, max_passes) trains an
 %   SVM classifier and returns trained model. X is the matrix of training 
 %   examples.  Each row is a training example, and the jth column holds the 
@@ -11,10 +12,10 @@ function [model] = svmTrain(X, Y, C, kernelFunction, ...
 %   parameter.  tol is a tolerance value used for determining equality of 
 %   floating point numbers. max_passes controls the number of iterations
 %   over the dataset (without changes to alpha) before the algorithm quits.
-%	训练一个SVM分类器并返回训练好的模型。X是训练集矩阵。每一行是一个训练样本，
-%	第j列记录第j个特征。Y是列向量，1与0表示类别。C是标准支持向量机正则化参数。
-%	tol是用于确定浮点数相等的公差值。在算法退出之前，max_passes控制数据集上的
-%	迭代次数（不改变alpha）。
+%	训练一个 SVM 分类器并返回训练好的模型。X 是训练集矩阵。每一行是一个训练样本，
+%	第 j 列记录第 j 个特征。Y 是列向量，1 与 0 表示类别。C 是标准支持向量机正则
+%	化参数。tol 是用于确定浮点数相等的容差值。在算法退出之前，max_passes控制数
+%	据集上的迭代次数（不改变 alpha的次数）。
 %
 % Note: This is a simplified version of the SMO algorithm for training
 %       SVMs. In practice, if you want to train an SVM classifier, we
@@ -27,10 +28,12 @@ function [model] = svmTrain(X, Y, C, kernelFunction, ...
 %
 %
 
+% tol 的默认值是0.001
 if ~exist('tol', 'var') || isempty(tol)
-    tol = 1e-3;
+    tol = 1e-3; 
 end
 
+% max_passes 的默认值是5
 if ~exist('max_passes', 'var') || isempty(max_passes)
     max_passes = 5;
 end
@@ -61,7 +64,7 @@ H = 0;
 if strcmp(func2str(kernelFunction), 'linearKernel')
     % Vectorized computation for the Linear Kernel
     % This is equivalent to computing the kernel on every pair of examples
-    K = X*X';
+    K = X * X';
 elseif strfind(func2str(kernelFunction), 'gaussianKernel')
     % Vectorized RBF Kernel
     % This is equivalent to computing the kernel on every pair of examples
@@ -86,17 +89,19 @@ dots = 12;
 while passes < max_passes, % 最大迭代次数以内
             
     num_changed_alphas = 0; % 发生alpha被改变的情况的迭代次数
-    for i = 1:m,
+    for i = 1:m, % 遍历数据集
         
         % Calculate Ei = f(x(i)) - y(i) using (2). 
         % E(i) = b + sum (X(i, :) * (repmat(alphas.*Y,1,n).*X)') - Y(i);
-        E(i) = b + sum (alphas .* Y .* K(:,i)) - Y(i);
-        
+		% 预测值f(x(i))是 b + sum(alphas .* Y .* K(:, i))
+        E(i) = b + sum(alphas .* Y .* K(:, i)) - Y(i);
+		% 选择 i 的条件
         if ((Y(i) * E(i) < -tol && alphas(i) < C) || (Y(i) * E(i) > tol && alphas(i) > 0)),
             
             % In practice, there are many heuristics one can use to select
             % the i and j. In this simplified code, we select them randomly.
-            j = ceil(m * rand()); % 随机选择j，保证j与i不同；ceil函数向右取整
+			% 实际运用中，有许多启发式的方法去选择 i 和 j，此处我们随机选择。
+            j = ceil(m * rand()); % 随机选择j，保证j与i不同；ceil函数是向右取整
             while j == i,  % Make sure i \neq j
                 j = ceil(m * rand());
             end
@@ -112,7 +117,7 @@ while passes < max_passes, % 最大迭代次数以内
             if (Y(i) == Y(j)),
                 L = max(0, alphas(j) + alphas(i) - C);
                 H = min(C, alphas(j) + alphas(i));
-            else
+            else % if (Y(i) != Y(j))
                 L = max(0, alphas(j) - alphas(i));
                 H = min(C, C + alphas(j) - alphas(i));
             end
@@ -123,29 +128,32 @@ while passes < max_passes, % 最大迭代次数以内
             end
 
             % Compute eta by (14).
-            eta = 2 * K(i,j) - K(i,i) - K(j,j);
+            eta = 2 * K(i, j) - K(i, i) - K(j, j);
             if (eta >= 0),
                 % continue to next i. 
-                continue;
+                continue;0
             end
             
             % Compute and clip new value for alpha j using (12) and (15).
-            alphas(j) = alphas(j) - (Y(j) * (E(i) - E(j))) / eta;
+			% 由目标函数对 alphas(j) 求偏导得：
+            alphas(j) = alphas(j) - (Y(j) * (E(i) - E(j))) / eta; 
             
             % Clip
+			% 为了 alphas(i) 符合 KKT 条件，确保 alphas(j) 在 H 与 L 之间.
             alphas(j) = min (H, alphas(j));
             alphas(j) = max (L, alphas(j));
             
             % Check if change in alpha is significant
-            if (abs(alphas(j) - alpha_j_old) < tol),
+            if (abs(alphas(j) - alpha_j_old) < tol), 
                 % continue to next i. 
-                % replace anyway
-                alphas(j) = alpha_j_old;
-                continue;
+                % replace anyway		 % 如果变化的幅度很小
+                alphas(j) = alpha_j_old; % alphas(j)不变化
+                continue;				 % 跳转下一循环
             end
             
             % Determine value for alpha i using (16). 
-            alphas(i) = alphas(i) + Y(i)*Y(j)*(alpha_j_old - alphas(j));
+			% 更新 alphas(i)
+            alphas(i) = alphas(i) + Y(i) * Y(j) * (alpha_j_old - alphas(j));
             
             % Compute b1 and b2 using (17) and (18) respectively. 
             b1 = b - E(i) ...
@@ -170,9 +178,9 @@ while passes < max_passes, % 最大迭代次数以内
         
     end
     
-    if (num_changed_alphas == 0),
+    if (num_changed_alphas == 0), % 此次迭代 alpha 未被修改
         passes = passes + 1;
-    else
+    else % 此次迭代 alpha 被修改
         passes = 0;
     end
 
@@ -195,6 +203,6 @@ model.y= Y(idx);
 model.kernelFunction = kernelFunction;
 model.b= b;
 model.alphas= alphas(idx);
-model.w = ((alphas.*Y)'*X)';
+model.w = ((alphas .* Y)' * X)';
 
 end
